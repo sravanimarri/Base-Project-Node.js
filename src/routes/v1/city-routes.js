b1bff57789cd09e264587c9724f130dfc81df33b
unknown <sravanimarri200@gmail.com>
2025-04-04 01:03:46 +0530

    implemenation of middlewares to the city

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,9 +1,11 @@
 const express =require('express');
 
 const {CityController}=require('../../controllers');
-
+const { CityMiddlewares }=require('../../middlewares');
 const router=express.Router();
 
-router.post('/',CityController.createCity);
+router.post('/',
+    CityMiddlewares.validateCreateRequest,
+    CityController.createCity);
 
 module.exports=router;
